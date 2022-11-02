a72d1480d14686e65aeb69740b5603aecfc41e0e
Abhishek-Baksi <abhisek.baksi@icloud.com>
2022-11-02 13:03:52 +0530

    variables have meningful names

--- prime.js
+++ prime.js
@@ -1,15 +1,15 @@
-function check_prime(num){
-    let count=0;
+function check_prime(number){
+    let factors=0;
     for(let i=1;i<=num;i++){
         if(num%i==0){
-            count++;
+            factors++ ;
         }
     }
-    if (count==2){
-     return true;
+    if(factors==2){
+        return true;
     }else{
-    return false;
-   }
+        return false;
+    }
 }
 let ans=check_prime(17);
-ans==true?console.log("Palindrome"):console.log("NA");
+ans==true?console.log("Number is a Palindrome"):console.log("Number is not a Palindrome");
